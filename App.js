136f26d4a519e831574fd895cfc23c7d7f12c069
objpedro <pedromusne.ti@gmail.com>
2022-03-16 17:24:07 -0300

    Upload do projeto com as animações inclusas

--- App.js
+++ App.js
@@ -1,18 +1,105 @@
-import React from 'react';
-import { View, KeyboardAvoidingView, Image, TextInput, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  KeyboardAvoidingView,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  Animated,
+  Keyboard
+} from 'react-native';
 
 export default function App() {
+
+  const [offset] = useState(new Animated.ValueXY({ x: 0, y: 95 }));
+  const [opacity] = useState(new Animated.Value(0));
+  const [logo] = useState(new Animated.ValueXY({ x: 130, y: 155 }));
+
+  useEffect(() => {
+
+    keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', keyboardDidShow);
+    keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', keyboardDidHide);
+
+    Animated.parallel([//inicia as animações simultaneamente
+
+      Animated.spring(offset.y, {//animação para o conteiner
+        toValue: 0,
+        speed: 4,
+        bounciness: 30,
+        useNativeDriver: true
+      }),
+
+      Animated.timing(opacity, {//animação visualizar conteiner
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: true
+      })
+
+    ]).start();//inicia as animações assim que iniciar o app
+
+  }, []);
+
+  function keyboardDidShow() {
+
+    Animated.parallel([
+      Animated.timing(logo.x, {
+        toValue: 55,
+        duration: 100,
+        useNativeDriver: false
+      }),
+
+      Animated.timing(logo.y, {
+        toValue: 65,
+        duration: 100,
+        useNativeDriver: false
+      }),
+    ]).start();
+
+  }
+
+  function keyboardDidHide() {
+
+    Animated.parallel([
+      Animated.timing(logo.x, {
+        toValue: 130,
+        duration: 100,
+        useNativeDriver: false
+      }),
+
+      Animated.timing(logo.y, {
+        toValue: 155,
+        duration: 100,
+        useNativeDriver: false
+      }),
+    ]).start();
+
+  }
+
   return (
     <KeyboardAvoidingView style={styles.background}>
       <View style={styles.conteinerLogo}>
 
-        <Image
+        <Animated.Image
+          style={{
+            width: logo.x,
+            height: logo.y,
+          }}
           source={require('./src/assets/logo.png')}
         />
 
       </View>
 
-      <View style={styles.conteiner}>
+      <Animated.View style={[
+        styles.conteiner,
+        {
+          opacity: opacity,
+          transform: [
+            { translateY: offset.y }
+          ]
+        }
+      ]}>
         <TextInput
           style={styles.input}
           placeholder='Email'
@@ -39,7 +126,7 @@ export default function App() {
           </Text>
         </TouchableOpacity>
 
-      </View>
+      </Animated.View>
     </KeyboardAvoidingView>
   );
 }
